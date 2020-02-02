e99a760b8c163767408720f5a21c23691ed117e0
Hubert Siwkin <hsiwkin@gmail.com>
2020-02-02 01:13:28 +0100

    Add registration

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const UserModel = require("../models/user.model");
+
+router.post("/register", async (req, res) => {
+  const saltRounds = 10;
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.sendStatus(401);
+  } else {
+    const found = await UserModel.find({ username });
+    if (found.length > 0) {
+      res.sendStatus(409);
+    } else {
+      const hash = await bcrypt.hash(password, saltRounds);
+      const newUser = await new UserModel({
+        username,
+        password: hash
+      }).save();
+
+      res.json({
+        username,
+        _id: newUser._id
+      });
+    }
+  }
+});
 
 router.post("/login", (req, res) => {
   // Mock user
